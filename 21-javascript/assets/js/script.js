52ed40d7241915be4c186dd4b558d2b37a47c637
Mohammed Fachihul Alam <fachirbl@gmail.com>
2024-03-27 00:18:38 +0600

    g

---
+++ 21-javascript/assets/js/script.js
@@ -0,0 +1,19 @@
+// var myName = "Md fachi";
+// alert(myName);
+var person = {
+    name: "Fachihul Alam",
+    age: 23,
+    profession: "Student",
+    showText: function () {
+        document.write("Hello there");
+    },
+};
+// alert(person.profession);
+// console.log(person.age);
+// console.log(person.showText());
+// person.showText();
+function alertFunc() {
+    // alert("Hi there");
+    document.write("Function has been executed");
+}
+// alertFunc();
